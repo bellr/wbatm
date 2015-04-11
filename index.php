f8d2eb63fc5f1d569bbd609d5e79fe1e02b4b712
atomly <atomly@mail.ru>
2015-04-11 20:38:56 +0300

    fix: admin route

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 require_once('header.php');
 
-Vitalis::Router();
+Vitalis::RouterAdmin();
--- protected/api/controllers/gcEasyPay.php
+++ protected/api/controllers/gcEasyPay.php
@@ -2,18 +2,25 @@
 class gcEasyPay {
 
     /**
-     * global call https://atm.wm-rb.net/api/EasyPay/webMoneyMerchant/
-     * local call Vitalis::Controller('EasyPay','webMoneyMerchant',array(),'gc');
+     * global call https://atm.wm-rb.net/api/EasyPay/getHistory/
+     * local call Vitalis::Controller('EasyPay','getHistory',array(),'gc');
      * @params $P->login
      * @params $P->mode
      */
-
     public function getHistory($P) {
 
         echo Extension::Payments()->EasyPay()->connect_history_easypay($P->login,$P->mode);
 
     }
 
+    /**
+     * global call https://atm.wm-rb.net/api/EasyPay/getHistory/
+     * local call Vitalis::Controller('EasyPay','getHistory',array(),'gc');
+     * @params $P->login
+     * @params $P->purse_in
+     * @params $P->in_val
+     * @params $P->did
+     */
     public function Translate($P) {
 
         echo Extension::Payments()->EasyPay()->direct_translation($P->login,$P->purse_in,trim(sprintf("%8.0f ",$P->in_val)),$P->did);
